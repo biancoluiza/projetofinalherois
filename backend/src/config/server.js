2889a4847e8eab8faf22870711513c40bc463eb2
LUIZA DUARTE BIANCO <41951@fiecalunos.lan>
2026-06-18 17:31:27 -0300

    adicionei os put mas falta testar eles

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -126,6 +126,102 @@ app.post('/missoes', async (req, res) => {
     }
 });
 
+app.put('/usuarios/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, email, senha } = req.body;
+
+        await db.query(
+            `UPDATE usuarios
+             SET nome = ?, email = ?, senha = ?
+             WHERE id_usuario = ?`,
+            [nome, email, senha, id]
+        );
+
+        res.json({
+            mensagem: 'Usuário atualizado com sucesso!'
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            erro: 'Erro ao atualizar usuário'
+        });
+    }
+});
+
+app.put('/guildas/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, descricao } = req.body;
+
+        await db.query(
+            `UPDATE guildas
+             SET nome = ?, descricao = ?
+             WHERE id_guilda = ?`,
+            [nome, descricao, id]
+        );
+
+        res.json({
+            mensagem: 'Guilda atualizado com sucesso!'
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            erro: 'Erro ao atualizar a guilda'
+        });
+    }
+});
+
+app.put('/herois/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { nome, classe, poder, avatar_url, id_guilda, id_usuario } = req.body;
+
+        await db.query(
+            `UPDATE herois
+             SET nome = ?, classe = ?, poder = ?, avatar_url = ?, id_guilda = ?, id_usuario = ?
+             WHERE id_heroi = ?`,
+            [nome, classe, poder, avatar_url, id_guilda, id_usuario, id]
+        );
+
+        res.json({
+            mensagem: 'Herói atualizado com sucesso!'
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            erro: 'Erro ao atualizar herói'
+        });
+    }
+});
+
+app.put('/missoes/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { descricao, recompensa_ouro, status } = req.body;
+
+        await db.query(
+            `UPDATE missoes
+             SET descricao = ?, recompensa_ouro = ?, status = ?
+             WHERE id_missoes = ?`,
+            [descricao, recompensa_ouro, status, id]
+        );
+
+        res.json({
+            mensagem: 'Missão atualizada com sucesso!'
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            erro: 'Erro ao atualizar missão'
+        });
+    }
+});
+
 app.listen(3000, () => {
     console.log('ligado')
 });
